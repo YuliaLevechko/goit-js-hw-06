561fe5a102f824ca0d47a544c684252bfac1fb24
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2023-07-25 21:48:27 +0300

    update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+const listIngredients = ingredients.map(ingredient => {
+  const listItems = document.createElement('li');
+  listItems.classList.add("item");
+  listItems.textContent = ingredient;
+  return listItems;
+});
+list.append(...listIngredients);
+console.log(list);
