6f9ecf8c9701ac6098e48c24604e192030f26f71
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2023-07-26 20:28:49 +0300

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesElements = document.querySelector('#boxes');
+const amountInput = document.querySelector('input');
+let arrayBox = [];
+
+createButton.addEventListener('click', createBoxes);
+function createBoxes(amount) { 
+  amount = amountInput.value;
+  let boxSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const randomColor = getRandomHexColor();
+    const box = `<div style="background-color: ${randomColor}; width: ${boxSize}px; height: ${boxSize}px; margin-bottom: 5px"></div>`
+    arrayBox.push(box);
+    boxSize += 10;
+  }
+  const boxes = arrayBox.join("");
+  boxesElements.innerHTML = boxes;
+}
+
+destroyButton.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  boxesElements.innerHTML = "";
+  arrayBox = [];
+  amountInput.value = 0;
+}
