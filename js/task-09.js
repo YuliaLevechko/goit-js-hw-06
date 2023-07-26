d0f8f3d05cd116de4746a93a9c5335282b97613c
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2023-07-26 19:15:26 +0300

    update task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const buttonElem = document.querySelector('.change-color');
+const nameColor = document.querySelector('.color');
+
+buttonElem.addEventListener('click', onchangeColor);
+function onchangeColor(event) {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  nameColor.textContent = randomColor;
+}
